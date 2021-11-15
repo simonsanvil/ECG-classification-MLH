95d5afdc0a0a2a967753fc769cbc4006ce73b5e3
Simon <simonsviloria@gmail.com>
2021-11-15 19:52:02 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Diagnose types of Arrhythmia from an ECG signale Learning models',
+    author='Group 2 - MLH',
+    license='MIT',
+)
